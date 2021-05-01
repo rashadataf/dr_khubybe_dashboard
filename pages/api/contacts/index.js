85c35a5c3f40fdcecadd2bdbad1a65252c8e51ff
Rashad Ataf <rashadattaf@gmail.com>
2021-05-01 16:58:23 +0300

    fixing post new contacts

--- pages/api/contacts/index.js
+++ pages/api/contacts/index.js
@@ -3,45 +3,44 @@ import { connectToDatabase } from "../../../util/mongodb";
 const allowCors = (fn) => async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader("Access-Control-Allow-Methods", "POST");
+  res.setHeader("Access-Control-Allow-Methods", "POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   return await fn(req, res);
 };
 
 async function handler(req, res) {
-  if (req.method === "POST") {
-    try {
-      const { client, db } = await connectToDatabase();
-      const contacts = db.collection("contacts");
-      const firstName = req.body.firstName;
-      const lastName = req.body.lastName;
-      const phone = req.body.phone;
-      const subject = req.body.subject;
-      const email = req.body.email;
-      if (email.length === 0) {
-        throw new Error("email can't be empty!");
-      } else {
-        let emailFilter = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-        let isEmail = emailFilter.test(email);
-        if (!isEmail) {
-          throw new Error("an invalid email!");
+  if (req.method === "OPTIONS") {
+    res.status(200).send(true);
+  } else {
+    if (req.method === "POST") {
+      try {
+        const { client, db } = await connectToDatabase();
+        const contacts = db.collection("contacts");
+        const firstName = req.body.firstName;
+        const lastName = req.body.lastName;
+        const phone = req.body.phone;
+        const subject = req.body.subject;
+        const email = req.body.email;
+        if (email.length === 0) {
+          throw new Error("email can't be empty!");
+        } else {
+          let emailFilter = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+          let isEmail = emailFilter.test(email);
+          if (!isEmail) {
+            throw new Error("an invalid email!");
+          }
         }
+        const result = await contacts.insertOne({
+          firstName,
+          lastName,
+          phone,
+          subject,
+          email,
+        });
+        res.status(200).send(true);
+      } catch (error) {
+        res.status(400).send({ error: "there was an error happened!" });
       }
-      const emails = db.collection("emails");
-      let isEmailInDB = await emails.findOne({ email: email });
-      if (isEmailInDB) {
-        throw new Error("email already exist's!");
-      }
-      const result = await contacts.insertOne({
-        firstName,
-        lastName,
-        phone,
-        subject,
-        email,
-      });
-      res.status(200).send(true);
-    } catch (error) {
-      res.status(400).send({ error: "there was an error happened!" });
     }
   }
 }
